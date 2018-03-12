415ee542599313a8c441bfcdea51e3461d899a15
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-12 16:54:45 -0400

    Modified PUT to return updated list of bears
    BearRouter.js --- chained a find off of findByIdAndUpdate.

--- bears/BearRouter.js
+++ bears/BearRouter.js
@@ -15,17 +15,17 @@ bearsRouter.get('/', (req, res) => {
 });
 
 bearsRouter.post('/', (req, res) => {
-    const bearInfo = req.body;
-    const bear = new Bear(bearInfo);
-    bear
-      .save()
-      .then(savedBear => {
-        res.status(201).json(savedBear);
-      })
-      .catch(err => {
-        res.status(500).json({ msg: 'Error saving bear', error: err });
-      });
-  });
+  const bearInfo = req.body;
+  const bear = new Bear(bearInfo);
+  bear
+    .save()
+    .then(savedBear => {
+      res.status(201).json(savedBear);
+    })
+    .catch(err => {
+      res.status(500).json({ msg: 'Error saving bear', error: err });
+    });
+});
 
 bearsRouter.get('/:id', (req, res) => {
   const { id } = req.params;
@@ -50,16 +50,16 @@ bearsRouter.delete('/:id', (req, res) => {
 });
 
 bearsRouter.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const updateInfo = req.body;
-    console.log(updateInfo)
-    Bear.findByIdAndUpdate(id, updateInfo)
-      .then(bear => {
-        res.status(200).json(bear);
-      })
-      .catch(err => {
-        res.status(500).json({ msg: 'Error getting bears', error: err });
-      });
-  });
+  const { id } = req.params;
+  const updateInfo = req.body;
+  Bear.findByIdAndUpdate(id, updateInfo)
+  .then(bears => Bear.find({}))
+    .then(bears => {
+      res.status(200).json(bears);
+    })
+    .catch(err => {
+      res.status(500).json({ msg: 'Error getting bears', error: err });
+    });
+});
 
 module.exports = bearsRouter;
