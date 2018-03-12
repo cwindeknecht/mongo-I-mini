b8bef6eeebe124ed4e53462f666f2a2f120af10c
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-12 16:43:59 -0400

    bear/:id PUT working
    BearRouter.js --- Added PUT method.

--- bears/BearRouter.js
+++ bears/BearRouter.js
@@ -4,20 +4,6 @@ const Bear = require('./BearModel.js');
 
 const bearsRouter = express.Router();
 
-bearsRouter.post('/', (req, res) => {
-  const bearInfo = req.body;
-  const bear = new Bear(bearInfo);
-
-  bear
-    .save()
-    .then(savedBear => {
-      res.status(201).json(savedBear);
-    })
-    .catch(err => {
-      res.status(500).json({ msg: 'Error saving bear', error: err });
-    });
-});
-
 bearsRouter.get('/', (req, res) => {
   Bear.find({})
     .then(bears => {
@@ -27,6 +13,19 @@ bearsRouter.get('/', (req, res) => {
       res.status(500).json({ msg: 'Error getting bears', error: err });
     });
 });
+
+bearsRouter.post('/', (req, res) => {
+    const bearInfo = req.body;
+    const bear = new Bear(bearInfo);
+    bear
+      .save()
+      .then(savedBear => {
+        res.status(201).json(savedBear);
+      })
+      .catch(err => {
+        res.status(500).json({ msg: 'Error saving bear', error: err });
+      });
+  });
 
 bearsRouter.get('/:id', (req, res) => {
   const { id } = req.params;
@@ -49,5 +48,18 @@ bearsRouter.delete('/:id', (req, res) => {
       res.status(500).json({ msg: 'Error getting bears', error: err });
     });
 });
+
+bearsRouter.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const updateInfo = req.body;
+    console.log(updateInfo)
+    Bear.findByIdAndUpdate(id, updateInfo)
+      .then(bear => {
+        res.status(200).json(bear);
+      })
+      .catch(err => {
+        res.status(500).json({ msg: 'Error getting bears', error: err });
+      });
+  });
 
 module.exports = bearsRouter;
